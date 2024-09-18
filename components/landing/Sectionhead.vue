05e364f92e11296f00e0330972f8479744fdcf61
CDFMLR <cdfmlr@163.com>
2024-09-18 20:59:01 +0800

    feat(contract): update contract information & page style

--- components/landing/Sectionhead.vue
+++ components/landing/Sectionhead.vue
@@ -7,12 +7,13 @@ defineProps({
 </script>
 
 <template>
-  <div :class="['mt-16', align === 'center' && 'text-center']">
-    <h1 class="text-4xl lg:text-5xl font-bold lg:tracking-tight">
+  <div :class="['grid', 'lg:grid-cols-1', 'place-items-center', 'pt-16', 'pb-8', 'md:pt-8', align === 'center' && 'text-center']">
+    <h1 class="text-5xl lg:text-6xl xl:text-7xl font-bold lg:tracking-tight">
       <slot name="title">Title</slot>
     </h1>
-    <p class="text-lg mt-4 text-slate-600">
+    <p class="text-lg mt-4 text-slate-600 max-w-xl">
       <slot name="desc">Some description goes here</slot>
+      <br /> <br />
     </p>
   </div>
 </template>
--- pages/contact.vue
+++ pages/contact.vue
@@ -8,39 +8,60 @@ definePageMeta({
   <LandingContainer>
     <LandingSectionhead>
       <template v-slot:title>Contact</template>
-      <template v-slot:desc>I am here to help.</template>
+      <template v-slot:desc>
+        murchinroom is glad to hear from you.
+        <br /><br />
+          If there are any questions or suggestions about our open-source projects,
+          please directly open an issue on the project's GitHub page.
+      </template>
     </LandingSectionhead>
 
-    <div class="grid md:grid-cols-2 gap-10 mx-auto max-w-4xl mt-16">
+    <div class="grid md:grid-cols-2 gap-10 mx-auto max-w-4xl mt-8">
       <div>
         <h2 class="font-medium text-2xl text-gray-800">
-          Contact Nuxtship Creator
+          Contact CDFMLR
         </h2>
         <p class="text-lg leading-relaxed text-slate-500 mt-3">
-          Have something to say? I am here to help. Fill up the form or send
-          email.
+          The main developer / maintainer of murchinroom projects
+          is happy to help you with any questions you have.
+          (And this person is available for hire).
         </p>
         <div class="mt-5">
           <div class="flex items-center mt-2 space-x-2 text-gray-600">
-            <Icon class="text-gray-400 w-4 h-4" name="uil:map-marker" />
-            <span>1010 Vienna, Austria</span>
+            <Icon class="text-gray-400 w-4 h-4" name="uil:map-marker"/>
+            <span>Earth, Solar System, Orion Arm, Milky Way</span>
           </div>
-          <div class="flex items-center mt-2 space-x-2 text-gray-600">
-            <Icon class="text-gray-400 w-4 h-4" name="uil:envelope" /><a
-              href="mailto:jakobaichmayr@gmail.com"
-              >jakobaichmayr@gmail.com</a
-            >
+          <div class="flex items-center mt-2 space-x-2 text-blue-600">
+            <Icon class="text-gray-400 w-4 h-4" name="uil:envelope"/>
+            <a class="underline font-mono font-bold" href="mailto:cdfmlr@murchinroom.fun">
+              cdfmlr@murchinroom.fun
+            </a>
           </div>
-          <!-- <div class="flex items-center mt-2 space-x-2 text-gray-600">
-            <Icon class="text-gray-400 w-4 h-4" name="uil:phone" /><a
-              href="tel:+1 (234) 5678 999"
-              >+1 (234) 5678 999</a
-            >
-          </div> -->
+          <!--          <div class="flex items-center mt-2 space-x-2 text-gray-600">-->
+          <!--            <Icon class="text-gray-400 w-4 h-4" name="uil:phone"/>-->
+          <!--            &lt;!&ndash;<a href="tel:+1 (234) 5678 999">+1 (234) 5678 999</a>&ndash;&gt;-->
+          <!--          </div>-->
         </div>
       </div>
       <div>
-        <LandingContactform />
+        <h2 class="font-medium text-2xl text-gray-800">
+          Available Services
+        </h2>
+        <p class="text-lg leading-relaxed text-slate-500 mt-3">
+          murchinroom & CDFMLR provides services in
+
+          software development,
+          high-performance computing,
+          cloud computing,
+          photography,
+          video editing,
+          music production,
+          poem writing,
+          and more.
+
+          If you have a project in mind, feel free to
+          contact us for a quote.
+        </p>
       </div>
     </div>
   </LandingContainer>
