dcbe8f6449d0e41274b9b0d35771faba900a7fea
CDFMLR <cdfmlr@murchinroom.fun>
2024-12-15 20:18:19 +0800

    feat(blogs): supports LaTeX formatted math within Markdown

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,6 +29,28 @@ export default defineNuxtConfig({
     },
     navigation: {
       fields: ['updated']
-    }
-  }
+    },
+    markdown: {
+      rehypePlugins: {
+        // "rehype-mathjax": {},
+        "rehype-katex": {
+          output: 'mathml' // https://github.com/nuxt/content/discussions/2561
+        }
+      },
+      remarkPlugins: [
+        'remark-math'
+      ],
+    },
+    highlight: {
+      // grep -IRn '```' | grep -v '```$' | grep '.md:' | grep -v '/plugins' | cut -d'`' -f4 | tr '[:upper:]' '[:lower:]' | sort | uniq | xargs
+      langs: ['asm', 'awk', 'bash', 'bibtex', 'c', 'c#', 'c++', 'cmd', 'ssh-config', 'cpp', 'cs', 'csharp', 'css', 'csv', 'dart', 'diff', 'docker', 'dockerfile', 'fish', 'go', 'html', 'http', 'ini', 'java', 'javascript', 'js', 'json', 'jsonl', 'latex', 'lisp', 'log', 'lua', 'make', 'makefile', 'markdown', 'matlab', 'md', 'mermaid', 'nginx', 'nix', 'perl', 'php', 'plsql', 'prolog', 'proto', 'protobuf', 'py', 'python', 'r', 'rust', 'sh', 'shell', 'shellscript', 'sql', 'swift', 'tex', 'toml', 'ts', 'typescript', 'vb', 'vue', 'xml', 'yaml', 'yml', 'zig', 'zsh'],
+      theme: {
+        default: 'github-light', // Default theme (same as single string)
+        dark: 'github-dark', // Theme used if `html.dark`
+        sepia: 'monokai', // Theme used if `html.sepia`
+      }
+    },
+  },
+
+  compatibilityDate: "2024-12-14"
 });
